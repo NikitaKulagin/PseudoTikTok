3b64aaee664c7bff60d78ed89be34ab18d01b55f
Никита Кулагин <nikitakulagin@MacBook-Pro-Nikita.local>
2025-01-23 15:13:26 +0300

    Add ARCHITECTURE.md with basic template

---
+++ PseudoTikTok/ARCHITECTURE.md
@@ -0,0 +1,10 @@
+# MyTikTokApp Architecture
+
+## Modules
+- Description of the main modules.
+
+## Services
+- Description of the services like Networking, Authentication, etc.
+
+## UI Components
+- Overview of the main UI components and view controllers.
---
+++ PseudoTikTok/PseudoTikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PseudoTikTok
+//
+//  Created by Никита Кулагин on 23.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
